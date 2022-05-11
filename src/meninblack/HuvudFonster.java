793136f01657afe878b3808923dfc99d9be2dddd
massed11 <105347180+massed11@users.noreply.github.com>
2022-05-11 10:32:49 +0200

    .

--- src/meninblack/HuvudFonster.java
+++ src/meninblack/HuvudFonster.java
@@ -39,6 +39,11 @@ public class HuvudFonster extends javax.swing.JFrame {
         jLabel1.setText("Hej");
 
         jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -63,6 +68,10 @@ public class HuvudFonster extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
