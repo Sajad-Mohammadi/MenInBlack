e05ce74d8095247b2a6fce57bd2f753e1de1c82c
Ali <105199844+sajadmohammmadi@users.noreply.github.com>
2022-05-13 13:02:44 +0200

    Update HuvudFonster.java

--- src/meninblack/HuvudFonster.java
+++ src/meninblack/HuvudFonster.java
@@ -133,13 +133,12 @@ public class HuvudFonster extends javax.swing.JFrame {
     private void btnLoggaInActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoggaInActionPerformed
         try {
             String losenord = new String(pwfLosenord.getPassword());
-            
-            String dbLosenord = idb.fetchSingle("Select losenord from agent where Namn='" + txfAnvandernamn.getText() + "'");
+            String dbLosenord = idb.fetchSingle("Select losenord from agent where Namn='" + txfAnvandernamn.getText()+"'");
             
             if(losenord.equals(dbLosenord)){
                 JOptionPane.showMessageDialog(rootPane, "Du är inloggad");
             } else{
-                JOptionPane.showMessageDialog(rootPane, "Något gick fel!", "", HEIGHT);
+                JOptionPane.showMessageDialog(rootPane, "Något gick fellllllll!", "", HEIGHT);
             }
         } catch (InfException e) {
             JOptionPane.showMessageDialog(null, "Något gick fel!");
